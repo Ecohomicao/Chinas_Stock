b686e23243e8dec3957098de97d740aa65d13a40
Ecohomicao <cxttxc2010@gmail.com>
2015-02-20 01:44:58 +0800

    Upload basic files

---
+++ Stock/spiders/stock_name.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from scrapy.spider import BaseSpider
+from Stock.items import NameItem
+from scrapy.selector import Selector
+from scrapy.http import Request
+
+class StockNameSpider(BaseSpider):
+    name = "stock_name"
+    #allowed_domains = ["http://app.finance.ifeng.com/list/"]
+    start_urls = ["http://app.finance.ifeng.com/list/stock.php?t=ha&f=symbol&o=asc&p=1",
+                  "http://app.finance.ifeng.com/list/stock.php?t=sa&f=symbol&o=asc&p=1"
+    ]
+    
+    def parse(self, response):
+        sel = Selector(response)
+        links = sel.xpath('//*[@class= "tab01"]/table/tr[position()>1]')
+        for link in links:
+            code = link.xpath('td[1]/a/text()').extract()
+            name = link.xpath('td[2]/a/text()').extract()
+            nameitem = NameItem()
+            if code <> [u'上一页' ,u'下一页'] and code <> [u'上一页'] and code <> [u'下一页'] and name <> [u'上一页' ,u'下一页'] and name <> [u'上一页'] and name <> [u'下一页']:
+                nameitem['code'] = code
+                nameitem['name'] = name
+            yield nameitem
+        for url in sel.xpath('//*[@class= "tab01"]/table/tr[52]/td/a/@href').extract():
+            url = "http://app.finance.ifeng.com/list/stock.php" + url
+            yield Request(url, callback = self.parse)
